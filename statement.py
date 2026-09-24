9a5bce99871ccfc8e0887953dbe4f28a520e1006
harshita976 <harshitat717@gmail.com>
2026-09-24 12:24:54 +0530

    Create statement.py

---
+++ statement.py
@@ -0,0 +1,28 @@
+"""
+SMART STUDY PLANNER
+-------------------
+
+Project Description:
+Smart Study Planner is a simple Python-based project
+that helps students create a basic study plan.
+
+Features:
+1. Takes student's name as input.
+2. Takes subject and topic as input.
+3. Takes available study hours.
+4. Creates a study plan according to study hours.
+5. Provides useful study tips.
+
+Concepts Used:
+- print()
+- input()
+- Variables
+- float()
+- if-elif-else
+- Basic Python programming
+
+Purpose:
+The purpose of this project is to help students
+organize their study time and practice Python
+programming concepts through a real-life project.
+"""
